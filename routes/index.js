b415a917a745e3118e45222c7e5054aec59f5e2a
casiodbx <casiodbx@hotmail.com>
2015-06-10 18:56:15 +0200

    Despliegue en Heroku

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,10 @@ var quizController = require('../controllers/quiz_controller');
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
+/*GET autores*/
+router.get('/autor', function(req, res) {
+  res.render('autor', { autor: 'Juan Carlos Álvarez Martín' });
+});
 
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer)
