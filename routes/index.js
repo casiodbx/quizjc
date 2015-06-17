88edcd90fde86182870d4b23903ef9750943895f
Juan Carlos <casiodbx@hotmail.com>
2015-06-17 20:37:34 +0200

    Multiples preguntas en quiz

--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,11 @@ router.get('/autor', function(req, res) {
   res.render('autor', { autor: 'Juan Carlos Álvarez Martín' });
 });
 
-router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer', quizController.answer)
+//router.get('/quizes/question', quizController.question);
+//router.get('/quizes/answer', quizController.answer)
+//Definíción de rutas de /quizes
+router.get('/quizes',						quizController.index);
+router.get('/quizes/:quizId(\\d+)',			quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer',	quizController.answer);
+
 module.exports = router;
