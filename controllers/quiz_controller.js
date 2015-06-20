f3ac5e650742a434d47b34822fcb08b1145e1d9d
Juan Carlos <casiodbx@hotmail.com>
2015-06-20 12:03:26 +0200

    Solución ejercicio modulo7

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -14,11 +14,17 @@ exports.load = function(req, res, next, quizId){
 
 //GET /quizes
 exports.index= function(req, res){
-	models.Quiz.findAll().then(function(quizes) {
-		res.render('quizes/index', {quizes: quizes});	
+	res.render('quizes/index', {});	
+};
+
+//GET /quizes/:search
+exports.listQuestion= function(req, res){
+	models.Quiz.findAll({where: ["pregunta like ?", req.search]}]).then(function(quizes) {
+		res.render('quizes/listQuestion', {quizes: quizes});	
 	}
   ).catch(function(error){ next(error);})
 };
+
 
 //GET /quizes/:id
 exports.show = function (req,res){
