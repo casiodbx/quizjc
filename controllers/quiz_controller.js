2fb25ef7c8b42ba2dc5c1ce89ae1758175fab04b
Juan Carlos <casiodbx@hotmail.com>
2015-06-23 18:09:04 +0200

    Incluir tratamiento de errores7

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -45,7 +45,7 @@ exports.answer = function (req,res){
 		if(req.query.respuesta===req.quiz.respuesta){
 			resultado='Correcto';
 		}
-		res.render('quizes/answer',{quiz:req.quiz, respuesta:resultado,errors[]]});
+		res.render('quizes/answer',{quiz:req.quiz, respuesta:resultado,errors:[]});
 };
 
 //GET /quizes/new
