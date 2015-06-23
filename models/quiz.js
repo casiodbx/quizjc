44c4cf6faac3a2fb85ba45ea3bc8e3201896ef16
Juan Carlos <casiodbx@hotmail.com>
2015-06-23 18:11:02 +0200

    Incluir tratamiento de errores10

--- models/quiz.js
+++ models/quiz.js
@@ -2,7 +2,7 @@
 module.exports =function(sequelize,DataTypes){
 	return sequelize.define('Quiz',
 		{	pregunta: {
-			type: DataTypes.STRING
+			type: DataTypes.STRING,
 			validate: {notEmpty: {msg: "-> Falta Pregunta"}}
 		},
 		respuesta: {
