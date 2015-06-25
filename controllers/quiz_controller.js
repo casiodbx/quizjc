6de16f1c8cf410a24f7bfed20036760bdfd006ac
Juan Carlos <casiodbx@hotmail.com>
2015-06-25 17:51:01 +0200

    modificacion controller3

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -60,14 +60,14 @@ exports.new = function(req, res){
 exports.create = function(req, res){
 	var quiz = models.Quiz.build(req.body.quiz);
 	quiz.validate().then(
-	    //function(err){
-	     /* if (err) {
+	    function(err){
+	     if (err) {
 	        res.render('quizes/new', {quiz: quiz, errors: err.errors});
 	      } else {
 	        quiz // save: guarda en DB campos pregunta y respuesta de quiz
 	        .save({fields: ["pregunta", "respuesta"]})
 	        .then( function(){ res.redirect('/quizes')}) 
-	      }      // res.redirect: Redirección HTTP a lista de preguntas*/
-	    //}
+	      }      // res.redirect: Redirección HTTP a lista de preguntas
+	    }
   	).catch(function(error){next(error)});	
 };
