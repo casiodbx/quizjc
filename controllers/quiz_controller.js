54fffbe9b991588697c56a0f17a0b3b6c685ef5a
Juan Carlos <casiodbx@hotmail.com>
2015-06-30 18:17:45 +0200

    borrar preguntas6

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -60,14 +60,14 @@ exports.new = function(req, res){
 exports.create = function(req, res){
 	var quiz = models.Quiz.build( req.body.quiz );
 
-	var errors = req.quiz.validate();//ya qe el objeto errors no tiene then(
+	var errors = quiz.validate();//ya qe el objeto errors no tiene then(
 	if (errors)
 		{
 			var i=0; var errores=new Array();//se convierte en [] con la propiedad message por compatibilida con layout
 			for (var prop in errors) errores[i++]={message: errors[prop]};
 			res.render('quizes/new', {quiz: quiz, errors: errores});
 		} else {
-			req.quiz // save: guarda en DB campos pregunta y respuesta de quiz
+			quiz // save: guarda en DB campos pregunta y respuesta de quiz
 			.save({fields: ["pregunta", "respuesta"]})
 			.then( function(){ res.redirect('/quizes')}) ;
 		}
