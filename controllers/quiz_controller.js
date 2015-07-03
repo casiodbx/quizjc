4b49927d723c8369f9da820d36a41a0b65e94d7f
Juan Carlos <casiodbx@hotmail.com>
2015-07-03 21:06:09 +0200

    Añadir comentarios a quiz v.1.4

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,7 +1,7 @@
 var models = require('../models/models.js');
 var temas = ["otro","humanidades","ocio","ciencia","tecnologia"];
 //Autoload -factoriza el código si ruta incluye :quizId
-exports.load = function(req, res, next, quizId){
+/*exports.load = function(req, res, next, quizId){
 	models.Quiz.find({
 			where: {id: Number(quizId) },
 			include: [{model: models.Comment}]
@@ -13,6 +13,24 @@ exports.load = function(req, res, next, quizId){
 			}else{next(new Error('No existe quizId='+quizId));}
 		}
 	).catch(function(error){next(error);});
+};*/
+
+// Autoload :id
+exports.load = function(req, res, next, quizId) {
+  models.Quiz.find({
+            where: {
+                id: Number(quizId)
+            },
+            include: [{
+                model: models.Comment
+            }]
+        }).then(function(quiz) {
+      if (quiz) {
+        req.quiz = quiz;
+        next();
+      } else{next(new Error('No existe quizId=' + quizId))}
+    }
+  ).catch(function(error){next(error)});
 };
 
 
