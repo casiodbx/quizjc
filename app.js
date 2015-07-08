1a34f1dae33ce7d937de798f435e3ff54552d3c4
Juan Carlos <casiodbx@hotmail.com>
2015-07-08 18:07:02 +0200

    Solucion autologout

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //Helpers dinamicos:
 app.use(function(req, res, next){
+  
     //guardar path en sesion.dredir para desues de login
     if(!req.path.match(/\/\login|\/logout/)){
         req.session.redir= req.path;
@@ -41,7 +42,30 @@ app.use(function(req, res, next){
     next();
 });
 
-app.use('/', routes);
+//app.use('/', routes);
+app.use('/', function(req, res, next) {
+
+    var now = new Date();
+    var stamp = req.session.time ? new Date(req.session.time) : new Date();
+
+    if (!req.path.match(/\/login|\/logout/)) {
+        // validamos tiempo ultima peticion > 2 minutos
+        if ((now.getMinutes() - 2) > stamp.getMinutes()) {
+            var errors = req.session.errors || 'Sesión caducada ...';
+            req.session.errors = {};
+            res.render('sessions/new', {
+            errors: errors
+            });
+        } else {
+            // refrescamos tiempo ultima peticion
+            req.session.time = new Date();
+            next();
+        }
+    } else {
+        next();
+    }
+
+}, routes);
 
 
 // catch 404 and forward to error handler
