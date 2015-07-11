8693683fba10b2165f8e41618fae0be4c273ba99
Juan Carlos <casiodbx@hotmail.com>
2015-07-11 19:36:24 +0200

    Añadir estadisticas V 3

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -120,9 +120,9 @@ exports.statistics=function(req, res){
 	var numeroComentarios=10;
 	var pgtasSinComentarios=2;
 	var mensaje;
-	mensaje='Número de preguntas : '+ numeroPgtas +'\n' +
-		  'Número de comentarios : '+ numeroComentarios +'\n'+
-		  'Número medio de comentarios por pregunta : '+(numeroPgtas/numeroComentarios)+' \n'
+	mensaje='Número de preguntas : '+ numeroPgtas +"\n" +
+		  'Número de comentarios : '+ numeroComentarios +"\n"+
+		  'Número medio de comentarios por pregunta : '+(numeroPgtas/numeroComentarios)+" \n"
 		  'Número de preguntas sin comentarios : '+pgtasSinComentarios+' \n'+	
 		  'Número de preguntas con comentarios : '+numeroPgtas-pgtasSinComentarios+' \n';	
 	res.render('statistics', {texto:mensaje,errors:[]}); 
